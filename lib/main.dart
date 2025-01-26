141fc93842c0e437f62c2153821d42e501c665c9
Karthikeya Panangipalli <karthikeya.panangipalli.02@gmail.com>
2025-01-26 15:09:55 -0800

    add workout_plan.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
-import 'pages/workout_history_page.dart'; // Update import path
+import 'package:provider/provider.dart';
+import 'models/workout_model.dart'; // Import workout model
+import 'workout_details/workout_history_page.dart'; // Import the workout history page // Import the workout recording page
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: WorkoutHistoryPage(), // Entry point updated to use the new structure
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => WorkoutProvider(),
+      child: MaterialApp(
+        title: 'Workout Tracker',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.teal, //AppBar teal color
+            titleTextStyle: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        home: WorkoutHistoryPage(),
+      ),
+    );
+  }
+}
+
+class WorkoutProvider with ChangeNotifier {
+  List<Workout> _workouts = [];
+
+  List<Workout> get workouts => _workouts;
+
+  void addWorkout(Workout workout) {
+    _workouts.add(workout);
+    notifyListeners();
+  }
 }
